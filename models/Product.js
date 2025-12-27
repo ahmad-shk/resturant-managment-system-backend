3137f4aa4ff701c15107a5c71e4f1775b96bea30
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2025-12-27 20:23:02 +0500

    udpate

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { type: String, required: true },
-    description: String,
-    isAvailable: { type: Boolean, default: true }
+    name: { 
+        type: String, 
+        required: [true, "Product ka naam lazmi hai"] 
+    },
+    price: { 
+        type: Number, 
+        required: [true, "Price batana zaroori hai"] 
+    },
+    category: { 
+        type: String, 
+        required: [true, "Category batana zaroori hai"] 
+    },
+    image: { 
+        type: String, 
+        required: [true, "Product ki image ka URL lazmi hai"] 
+    },
+    description: { 
+        type: String, 
+        required: [true, "Description likhna lazmi hai"] 
+    },
+    isAvailable: { 
+        type: Boolean, 
+        required: [true, "Availability status batana zaroori hai"],
+        default: true 
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
