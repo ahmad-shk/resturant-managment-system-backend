acb58042651b059188f16d139018af304ea2ea20
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2025-12-27 20:23:44 +0500

    udpate

--- models/Product.js
+++ models/Product.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: { 
         type: String, 
-        required: [true, "Product ka naam lazmi hai"] 
+        required: [true, "Product name is required"] 
     },
     price: { 
         type: Number, 
-        required: [true, "Price batana zaroori hai"] 
+        required: [true, "Product price is required"] 
     },
     category: { 
         type: String, 
-        required: [true, "Category batana zaroori hai"] 
+        required: [true, "Product category is required"] 
     },
     image: { 
         type: String, 
-        required: [true, "Product ki image ka URL lazmi hai"] 
+        required: [true, "Product image URL is required"] 
     },
     description: { 
         type: String, 
-        required: [true, "Description likhna lazmi hai"] 
+        required: [true, "Product description is required"] 
     },
     isAvailable: { 
         type: Boolean, 
-        required: [true, "Availability status batana zaroori hai"],
+        required: [true, "Availability status is required"],
         default: true 
     }
 }, { timestamps: true });
